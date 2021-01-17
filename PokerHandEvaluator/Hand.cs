09c71bbf337c98d48909d01fd967f245de155355
hughes03soft <hughes03.soft@gmail.com>
2021-01-18 01:54:25 +0800

    added Hands.cs

---
+++ PokerHandEvaluator/Hand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace PokerHandEvaluator
+{
+    class Hand
+    {
+        public string Owner { get; private set; }
+        public string[] RawCards { get; private set; }
+        public List<Card> Cards { get; private set; } = new List<Card>();
+        public string Description { get; set; }
+        public int RankScore { get; set; }
+
+        public Hand(string owner, string[] cards)
+        {
+            Owner = owner;
+            RawCards = cards;
+
+            foreach (var card in cards)
+            {
+                Cards.Add(new Card(card));
+            }
+        }
+    }
+}
