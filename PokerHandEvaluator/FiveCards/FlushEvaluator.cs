d8df866320cbbd1208eb7cb060f6b17c6efb7641
hughes03soft <hughes03.soft@gmail.com>
2021-01-18 03:59:56 +0800

    added FlushEvaluator class and test

---
+++ PokerHandEvaluator/FiveCards/FlushEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PokerHandEvaluator.FiveCards
+{
+    public class FlushEvaluator
+    {
+        public string Description => "Flush";
+
+        public bool IsValidCombination(Hand hand)
+        {
+            int andedSuites = 0xFFFF;
+
+            foreach (var card in hand.Cards)
+                andedSuites &= (int)card.Suite;
+
+            if ((andedSuites & Card.SUITE_MASK) > 0)
+                return true;
+
+            return false;
+        }
+
+        public int CalculateRankScore(Hand hand)
+        {
+            int rankScore = (int)FiveCardPokerEvaluator.HandRank.Flush +
+                            (int)hand.Cards.Max();
+
+            return rankScore;
+        }
+    }
+}
---
+++ PokerHandEvaluatorTests/FiveCards/FlushEvaluatorTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHandEvaluator.FiveCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandEvaluator.FiveCards.Tests
+{
+    [TestClass()]
+    public class FlushEvaluatorTests
+    {
+        FlushEvaluator Eval = new FlushEvaluator();
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetIsValidCombinationTestData), DynamicDataSourceType.Method)]
+        public void IsValidCombinationTest(Hand hand, bool expected)
+        {
+            bool actual = Eval.IsValidCombination(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetIsValidCombinationTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "3C", "6C", "KC", "7C" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AS", "3S", "6S", "KS", "7S" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AD", "3D", "6D", "KD", "7D" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AH", "3H", "6H", "KH", "7H" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AH", "3H", "6S", "KH", "7H" }), false };
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "3H", "6H", "KD", "7H" }), false };
+        }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetCalculateRankScoreTestTestData), DynamicDataSourceType.Method)]
+        public void CalculateRankScoreTest(Hand hand, int expected)
+        {
+            int actual = Eval.CalculateRankScore(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetCalculateRankScoreTestTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "3C", "6C", "KC", "7C" }), (int)FiveCardPokerEvaluator.HandRank.Flush + (int)Card.Values.Ace };
+            yield return new object[] { new Hand("Player1", new string[] { "5S", "3S", "6S", "9S", "7S" }), (int)FiveCardPokerEvaluator.HandRank.Flush + (int)Card.Values.Nine };
+        }
+    }
+}
