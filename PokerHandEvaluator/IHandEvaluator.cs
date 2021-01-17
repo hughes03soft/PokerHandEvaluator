f4381e32a6e46dda61035ba05972389dc7fdbe8c
hughes03soft <hughes03.soft@gmail.com>
2021-01-18 01:57:08 +0800

    added IHandEvaluator

---
+++ PokerHandEvaluator/IHandEvaluator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandEvaluator
+{
+    public interface IHandEvalutor
+    {
+        string Description { get; }
+        bool IsValidCombination(Hand hand);
+        int CalculateRankScore(Hand hand);
+    }
+}
