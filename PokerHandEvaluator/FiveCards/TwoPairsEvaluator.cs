137d28b93af524b13e1cb3f66be4a0201acaac73
hughes03soft <hughes03.soft@gmail.com>
2021-01-18 14:52:35 +0800

    added Two Pairs Evaluator class and tests

--- PokerHandEvaluator/FiveCards/TwoPairsEvaluator.cs
+++ PokerHandEvaluator/FiveCards/TwoPairsEvaluator.cs
@@ -8,16 +8,38 @@ namespace PokerHandEvaluator.FiveCards
 {
     public class TwoPairsEvaluator : IHandEvaluator
     {
-        public string Description => throw new NotImplementedException();
-
-        public int CalculateRankScore(Hand hand)
-        {
-            throw new NotImplementedException();
-        }
+        private const int EXPECTED_UNIQUE_CARD_VALUES = 3;
+        private const int EXPECTED_MAX_DUPLICATE_COUNT = 2;
+        public string Description => "Two Pairs";
 
         public bool IsValidCombination(Hand hand)
         {
-            throw new NotImplementedException();
+            return hand.UniqueCardValues.Count == EXPECTED_UNIQUE_CARD_VALUES &&
+                   hand.CardValueCount(hand.CardValueOfMaxDuplicateCount) == EXPECTED_MAX_DUPLICATE_COUNT;
         }
+
+        public int CalculateRankScore(Hand hand)
+        {
+            int rankScore = (int)FiveCardPokerEvaluator.HandRank.TwoPairs;
+            //highest 2 msb is currently free
+            // shift left CardValue once for the pairs
+            // rankScore = shifted(1) + non-pair
+
+            foreach (var value in hand.UniqueCardValues)
+            {
+                if (hand.CardValueCount(value) == EXPECTED_MAX_DUPLICATE_COUNT)
+                {
+                    rankScore += ((int)value << 1);
+                }
+                else
+                {
+                    rankScore += (int)value;
+                }
+            }
+
+            return rankScore;
+        }
+
+
     }
 }
---
+++ PokerHandEvaluatorTests/FiveCards/TwoPairsEvaluatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHandEvaluator.FiveCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandEvaluator.FiveCards.Tests
+{
+    [TestClass()]
+    public class TwoPairsEvaluatorTests
+    {
+        TwoPairsEvaluator Eval = new TwoPairsEvaluator();
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetIsValidCombinationTestData), DynamicDataSourceType.Method)]
+        public void IsValidCombinationTest(Hand hand, bool expected)
+        {
+            bool actual = Eval.IsValidCombination(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetIsValidCombinationTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "AS", "KH", "KD", "10C" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "7C", "7S", "3H", "3D", "JC" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AH", "AH", "AS", "KH", "7H" }), false };
+            yield return new object[] { new Hand("Player1", new string[] { "2C", "2H", "2H", "2D", "6H" }), false };
+        }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetCalculateRankScoreTestTestData), DynamicDataSourceType.Method)]
+        public void CalculateRankScoreTest(Hand hand, int expected)
+        {
+            int actual = Eval.CalculateRankScore(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetCalculateRankScoreTestTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "AS", "KH", "KD", "10C" }), (int)FiveCardPokerEvaluator.HandRank.TwoPairs + (((int)Card.Values.Ace << 1) | ((int)Card.Values.King << 1) | ((int)Card.Values.Ten)) };
+            yield return new object[] { new Hand("Player1", new string[] { "7C", "7S", "3H", "3D", "JC" }), (int)FiveCardPokerEvaluator.HandRank.TwoPairs + (((int)Card.Values.Seven << 1) | ((int)Card.Values.Three << 1) | ((int)Card.Values.Jack)) };
+        }
+    }
+}
