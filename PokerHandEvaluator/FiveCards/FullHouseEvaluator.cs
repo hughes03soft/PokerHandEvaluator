7e3e4ef3d54ecb2ceb192f19941368a12980e147
hughes03soft <hughes03.soft@gmail.com>
2021-01-18 11:59:10 +0800

    added fullhouse evaluator class and tests, added three of a kind evaluator place-holder

---
+++ PokerHandEvaluator/FiveCards/FullHouseEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandEvaluator.FiveCards
+{
+    public class FullHouseEvaluator : IHandEvaluator
+    {
+        private int EXPECTED_UNIQUE_CARD_VALUES = 2;
+        private int EXPECTED_MAX_DUPLICATE_COUNT = 3;
+        public string Description => "Full House";
+        public bool IsValidCombination(Hand hand)
+        {
+            return hand.UniqueCardValues.Count == EXPECTED_UNIQUE_CARD_VALUES &&
+                   hand.CardValueCount(hand.CardValueOfMaxDuplicateCount) == EXPECTED_MAX_DUPLICATE_COUNT;
+        }
+
+        public int CalculateRankScore(Hand hand)
+        {
+            int rankScore = (int)FiveCardPokerEvaluator.HandRank.FullHouse +
+                            (int)hand.CardValueOfMaxDuplicateCount;
+
+            return rankScore;
+        }
+    }
+}
---
+++ PokerHandEvaluatorTests/FiveCards/FullHouseEvaluatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHandEvaluator.FiveCards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandEvaluator.FiveCards.Tests
+{
+    [TestClass()]
+    public class FullHouseEvaluatorTests
+    {
+        FullHouseEvaluator Eval = new FullHouseEvaluator();
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetIsValidCombinationTestData), DynamicDataSourceType.Method)]
+        public void IsValidCombinationTest(Hand hand, bool expected)
+        {
+            bool actual = Eval.IsValidCombination(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetIsValidCombinationTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "AS", "AH", "AD", "10C" }), false };
+            yield return new object[] { new Hand("Player1", new string[] { "7C", "7S", "7H", "9D", "9C" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "AH", "AH", "AS", "KH", "KH" }), true };
+            yield return new object[] { new Hand("Player1", new string[] { "2C", "2H", "4H", "4D", "6H" }), false };
+        }
+
+        [DataTestMethod()]
+        [DynamicData(nameof(GetCalculateRankScoreTestTestData), DynamicDataSourceType.Method)]
+        public void CalculateRankScoreTest(Hand hand, int expected)
+        {
+            int actual = Eval.CalculateRankScore(hand);
+            Assert.AreEqual(expected, actual);
+        }
+
+        public static IEnumerable<object[]> GetCalculateRankScoreTestTestData()
+        {
+            yield return new object[] { new Hand("Player1", new string[] { "AC", "AS", "AH", "10D", "10C" }), (int)FiveCardPokerEvaluator.HandRank.FullHouse + (int)Card.Values.Ace };
+            yield return new object[] { new Hand("Player1", new string[] { "7C", "7S", "7H", "9D", "9C" }), (int)FiveCardPokerEvaluator.HandRank.FullHouse + (int)Card.Values.Seven };
+        }
+    }
+}
